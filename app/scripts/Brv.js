e405c62d5404ddd0b5ba726a0b184a8fa0cde5ee
Alejandro Morales <vamg008@gmail.com>
2013-04-06 13:35:18 -0600

    [fix] Unstaged change

--- app/scripts/Brv.js
+++ app/scripts/Brv.js
@@ -12,29 +12,3 @@ var Brv = Em.Application.create({
     name: 'Ejecutivos 5 Estrellas'
   })
 })
-
-Brv.Router.map(function(){
-  this.resource('login')
-  this.resource('home', function (){
-    this.resource('day', {path:':day'})
-  })
-})
-
-Brv.IndexRoute = Em.Route.extend({
-  redirect: function (){
-    this.transitionTo('login')
-  }
-})
-
-Brv.HomeRoute = Em.Route.extend({
-  model: function(){
-    return Brv.loggedUser || Brv.User.create({})
-  }
-})
-
-Brv.LoginRoute = Em.Route.extend({
-  model: function (){
-    return Brv.about
-  }
-})
-
